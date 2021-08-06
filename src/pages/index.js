b8c0f10a690b40bbb7ff1a7da607e4647e932bf9
Nicolas Garcia <nicolasgarcia214@gmail.com>
2021-08-06 14:48:18 -0500

    📂 Set folder Structure
    - Installing Sass
    - Separate components
    - Erase Data

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Dextter</title>
+        <meta name="description" content="We can do better 💡" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </div>
+  );
+}
